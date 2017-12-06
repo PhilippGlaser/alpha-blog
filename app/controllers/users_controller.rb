7ad78988ef568b9dd64806d521bbf81b209c0515
PhilippGlaser <pglaser21@gmail.com>
2017-12-06 04:29:33 +0000

    small bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:succes] = "Welcome to the Alpha/Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -47,7 +48,7 @@ class UsersController < ApplicationController
   end
   def require_same_user
     if current_user != @user
-      flash[:danger] "You can only edit your own account"
+      flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
   end
